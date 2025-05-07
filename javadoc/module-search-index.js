e57adf7ce7fb67febd2ceae6f1eb445d762328a0
Srain3 <srain3002@gmail.com>
2025-05-07 09:28:40 +0900

    Dokka_doc update
    Dokkaドキュメントへ移行&最新のdocへ更新

--- javadoc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-var moduleSearchIndex = [{"l":"SrainLib","url":"index.html"}]
--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-var packageSearchIndex = [{"l":"com.github.srain3.plugin.lib","url":"com/github/srain3/plugin/lib/package-summary.html"}, {"l":"All packages","url":"index.html"}]
